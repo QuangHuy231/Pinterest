7d14a2dc39ac46fff1364b89b91487139c70d28f
Quang Huy <nguyenquanghuya3kd@gmail.com>
2023-02-10 20:49:00 +0700

    login with google

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,64 @@
 import React from "react";
 
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+import { GoogleLogin } from "@react-oauth/google";
+import jwtDecode from "jwt-decode";
+import { client } from "../client";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleSuccess = (res) => {
+    const decoded = jwtDecode(res.credential);
+    localStorage.setItem("user", JSON.stringify(decoded));
+    const { name, sub, picture } = decoded;
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleOAuthProvider
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+            >
+              <GoogleLogin
+                onSuccess={handleSuccess}
+                onError={() => console.log("Error")}
+              />
+            </GoogleOAuthProvider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
